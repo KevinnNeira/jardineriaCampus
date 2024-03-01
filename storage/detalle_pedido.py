c1e6f7d0709c6da9b763ec78b8e3c12633784682
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-03-01 09:25:31 -0500

    jardineria

---
+++ storage/detalle_pedido.py
@@ -0,0 +1,18 @@
+detalle_pedido = [
+    {
+        "codigo_pedido": 0,
+        "Codigo_producto": "",
+        "cantidad": 0,
+        "precio_unidad": 1.0,
+        "numero_linera": 2.0,
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+]
---
+++ storage/producto.py
@@ -0,0 +1,13 @@
+producto = [
+    {
+        "Codigo_producto": "",
+        "nombre": "",
+        "gama": "",
+        "dimensiones": "",
+        "proveedor": "",
+        "descripcion": "",
+        "cantidad_en__stock": 0,
+        "precio_venta": 1.0,
+        "precio": 1.0,        
+    }
+]
