5148849c80427ec87d490634c1b1bf533ff805a0
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-03-04 09:09:10 -0500

    jardineria v2

--- modules/getClients.py
+++ modules/getClients.py
@@ -1,4 +1,10 @@
-from storage.cliente import cliente
+import modules.getClients as cli
 
-def searchName():
-    print(cliente)
+def search():
+    clientName: []
+    for i, val in enumerate(cli.cliente):
+        clientName.append({
+            "nombre_cliente": val.get("nombre_cliente"),
+            "codigo_cliente": val.get("codigo_cliente")
+        })
+    return clientName
